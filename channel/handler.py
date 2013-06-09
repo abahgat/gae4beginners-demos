32a5d57941a69fc31ed7119281e9aef560d61a1f
Alessandro Bahgat <ale.bahgat@gmail.com>
2013-06-09 18:00:56 +0200

    Multichannel support and presence notifications

--- channel/handler.py
+++ channel/handler.py
@@ -7,29 +7,67 @@ import uuid
 import webapp2
 
 from google.appengine.api import channel
+from google.appengine.api import memcache
 
 class CursorHandler(webapp2.RequestHandler):
+
+  def _render_template(self, template_values):
+    template = main.jinja_environment.get_template(
+        os.path.join('channel', 'index.html'))
+    self.response.out.write(template.render(template_values))
+
   def get(self):
     client_id = uuid.uuid4().hex
 
     token = channel.create_channel(client_id)
 
     template_values = {
-      'client_id': client_id,
+      'client': client_id,
       'token': token
     }
-    template = main.jinja_environment.get_template(
-        os.path.join('channel', 'index.html'))
-    self.response.out.write(template.render(template_values))
+    self._render_template(template_values)
 
   def post(self):
     x = self.request.get('x')
     y = self.request.get('y')
-    client_id = self.request.get('client_id')
+    client_id = self.request.get('client')
 
     message = {
       'x': x,
       'y': y,
       'client': client_id,
     }
-    channel.send_message(client_id, json.dumps(message))
+
+    clients = memcache.get('clients')
+    if clients:
+      for client in clients:
+        channel.send_message(client, json.dumps(message))
+
+class ConnectHandler(webapp2.RequestHandler):
+  """Handler that will be called when a new client connects."""
+
+  def post(self):
+    client_id = self.request.get('from')
+
+    clients = memcache.get('clients')
+    if clients is None:
+      clients = set([client_id])
+    else:
+      clients.add(client_id)
+
+    memcache.set('clients', clients)
+    import logging
+    logging.error(memcache.get('clients'))
+
+class DisconnectHandler(webapp2.RequestHandler):
+  """Handler that will be called when a client disconnects."""
+
+  def post(self):
+    client_id = self.request.get('from')
+    return None
+
+    clients = memcache.get('clients')
+    if clients is not None:
+      clients.remove(client_id)
+
+    memcache.set('clients', clients)
--- main.py
+++ main.py
@@ -10,5 +10,7 @@ jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/channel', 'channel.handler.CursorHandler')
+    ('/channel', 'channel.handler.CursorHandler'),
+    ('/_ah/channel/connected/', 'channel.handler.ConnectHandler'),
+    ('/_ah/channel/disconnected/', 'channel.handler.DisconnectHandler'),
 ], debug=True)
